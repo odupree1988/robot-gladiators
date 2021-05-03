f05c2c404e1ab86d9ba901820fc4c0f322c9b2c7
odupree1988 <odupree1988@gmail.com>
2021-05-03 15:05:38 -0400

    added robot name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! Javascript is running!");
+// this creates a function called "fight"
+
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10+10);
+console.log("Our robot's name is " +playerName);
+
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+// fight();
